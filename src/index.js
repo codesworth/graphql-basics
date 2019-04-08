5c6082d812b5103018350aef71e51ea9a4823bf8
Codesworth <codezworth@gmail.com>
2019-04-08 01:46:15 +0000

    Initialized Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+//type Definitions :: Applcation Schema
+const typedefs = `
+    type Query{
+        hello: String!
+        name:String!
+        bio:String
+    }
+`;
+
+//Resolvers:: Set of FUnctions for each action
+const resolvers = {
+  Query: {
+    hello() {
+      return "First query!";
+    },
+
+    name() {
+      return "jeremie Njitap";
+    },
+    bio() {
+      return null;
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs: typedefs, resolvers: resolvers });
+server.start(() => {
+  console.log("GraphQL Server is Live");
+});
