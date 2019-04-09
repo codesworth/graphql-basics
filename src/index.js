f6ec517255cfb60c194001db71a61241018fa0d7
Codesworth <codezworth@gmail.com>
2019-04-09 07:25:50 +0000

    Added Custom types and Query

--- src/index.js
+++ src/index.js
@@ -1,26 +1,57 @@
 import { GraphQLServer } from "graphql-yoga";
 
 //type Definitions :: Applcation Schema
+
+//Scaler Types: String, Boolean, Int, Float, ID
 const typedefs = `
     type Query{
-        hello: String!
-        name:String!
-        bio:String
+      greeting(name: String): String!
+      me: User!
+      post:Post!
+    }
+
+    type User{
+      id: ID
+      name: String!
+      email:String!
+      age: Int
+    }
+
+    type Post{
+      id: ID!
+      published: Boolean
+      title: String
+      body:String
     }
 `;
 
 //Resolvers:: Set of FUnctions for each action
 const resolvers = {
   Query: {
-    hello() {
-      return "First query!";
+    me() {
+      return {
+        id: "msnsmdnkdksdm",
+        name: "Jerry Poona",
+        age: 45,
+        email: "Addon Save"
+      };
     },
 
-    name() {
-      return "jeremie Njitap";
+    post() {
+      return {
+        id: "Sjkjs-dsnnsd",
+        title: "GrapphQL",
+        published: false,
+        body:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores consectetur eveniet sunt quia officiis aperiam, blanditiis quaerat repellat ex inventore aliquid beatae ipsa saepe vero vel soluta! Repudiandae, voluptas ex!"
+      };
     },
-    bio() {
-      return null;
+
+    greeting(parent, args, ctx, info) {
+      if (args.name) {
+        return `Hello There ${args.name}`;
+      }
+      return "Hello";
     }
   }
 };
